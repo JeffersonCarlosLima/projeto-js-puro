84a0332ad09f7cf9bf61b69dae5228e46d93616b
JeffersonCarlosLima <jeffersoncl200@gmail.com>
2023-10-03 13:27:08 -0300

    commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+class Produto{
+
+    constructor(){
+        this.id = 0;
+        this.nomeProduto = '';
+        this.valor = 0;
+    }
+    adicionar(){
+        alert("metodo chamado");
+    }
+    excluir(){
+        alert("metodo chamado");
+    }
+}
+
+var produto = new Produto();
